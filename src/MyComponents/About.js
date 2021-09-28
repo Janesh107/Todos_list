722cf8794022e5a778f11f06d32917fbb8c682ea
Janesh Kumar Chhaniwal <56973317+Janesh107@users.noreply.github.com>
2021-09-28 05:45:42 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <>
+        <div className="container">
+           <h2>About Todo List </h2>
+           <p>It is basically an which helps you to note down the list the things you have to do and the things which has been done by you.</p>
+
+        </div>
+        </>
+    )
+}
